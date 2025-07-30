aed4048a796fe6bf32bd95d9a0c7374a6edb8181
AMMI SAID Faiçal <70859943+AMMISAIDFaical@users.noreply.github.com>
2025-07-30 15:43:49 +0000

    fixing tf

--- smart_driving_school/src/basics/docker.tf
+++ smart_driving_school/src/basics/docker.tf
@@ -1,10 +1,17 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
-    }
-  }
+// This is the same as doing:
+// docker pull hashicorp/vault:1.12.6
+resource "docker_image" "vault" {
+  name = "hashicorp/vault:1.12.6"
 }
 
-provider "docker" {}
+// This is the same as doing:
+// docker run -p 8200:8200 --name "terraform-basics-vault" hashicorp/vault:1.12.6
+resource "docker_container" "vault" {
+  name  = "terraform-basics-vault"
+  image = docker_image.vault.image_id
+
+  ports {
+    internal = 8200
+    external = 8200
+  }
+}
