c7dcc3502a5528c01aeeca152d36cc76b1e24652
AMMISAIDFaiçal <70859943+AMMISAIDFaical@users.noreply.github.com>
2025-07-31 14:46:02 +0000

    outputs

---
+++ infra/outputs.tf
@@ -0,0 +1,16 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "azurerm_search_service_name" {
+  value = azurerm_search_service.search.name
+}
+
+output "azurerm_search_service_id" {
+  value = azurerm_search_service.search.id
+}
+
+output "azurerm_search_service_primary_key" {
+  value     = azurerm_search_service.search.primary_key
+  sensitive = true
+}
