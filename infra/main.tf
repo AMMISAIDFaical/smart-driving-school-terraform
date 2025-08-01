ea852a3caa63cf45d456e49765ed2604ebd9cbdf
AMMISAIDFaiçal <70859943+AMMISAIDFaical@users.noreply.github.com>
2025-08-01 17:44:19 +0000

    resources created for az search and env vars saved

--- infra/main.tf
+++ infra/main.tf
@@ -1,14 +1,39 @@
-
 resource "azurerm_resource_group" "rg" {
-  name     = "az_ai_search_rg"
+  name     = "az_ai_search_drvsh_rg"
   location = var.resource_group_location
 }
 
 resource "azurerm_search_service" "search" {
-  name                = "azaisearchservice"
+  name                = "azureaisearchservicedrvschool"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = var.sku
   replica_count       = var.replica_count
   partition_count     = var.partition_count
+}
+
+resource "azurerm_storage_account" "drvschstrg" {
+  name                     = "drvschstrg"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS" 
+  access_tier              = "Hot" 
+}
+
+
+resource "azurerm_storage_container" "drvschoolcontainer" {
+  name                  = "drvschoolcontainer"
+  storage_account_name  = azurerm_storage_account.drvschstrg.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "drv_blobs" {
+  for_each = fileset(path.module, "../smart_driving_school/data/*")
+
+  name                   = trim(each.key, "../smart_driving_school/data/")
+  storage_account_name   = azurerm_storage_account.drvschstrg.name
+  storage_container_name = azurerm_storage_container.drvschoolcontainer.name
+  type                   = "Block"
+  source                 = each.key
 }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -9,3 +9,21 @@ output "azurerm_search_service_name" {
 output "azurerm_search_service_id" {
   value = azurerm_search_service.search.id
 }
+
+output "AZURE_STORAGE_CONNECTION_STRING" {
+  value     = azurerm_storage_account.drvschstrg.primary_connection_string
+  sensitive = true
+}
+
+output "AZURE_CONTAINER_NAME" {
+  value = azurerm_storage_container.drvschoolcontainer.name
+}
+
+output "AZURE_OPENAI_ENDPOINT" {
+  value = azurerm_cognitive_account.openai.endpoint
+}
+
+output "AZURE_OPENAI_API_KEY" {
+  value     = azurerm_cognitive_account.openai.primary_access_key
+  sensitive = true
+}
